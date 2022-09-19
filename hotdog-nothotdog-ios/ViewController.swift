18cbe4380e12c7b75eadb657e41659535417e014
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-09-19 14:36:38 -0400

    Initial Commit

---
+++ hotdog-nothotdog-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotdog-nothotdog-ios
+//
+//  Created by robertsimoes on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
